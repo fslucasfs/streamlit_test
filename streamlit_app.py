b7773f44579121947a94228382e812d3f28565ef
fslucasfs <lucasdotfs2@gmail.com>
2024-11-22 10:53:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import streamlit as st
 
+# Input principal
+input_principal = st.selectbox("Selecione uma opção:", ["Selecione", "Opção 1", "Opção 2"])
+
+# Mostrar outro input dependendo do valor selecionado
+if input_principal == "Opção 1":
+    st.text_input("Insira algo para Opção 1:")
+elif input_principal == "Opção 2":
+    st.number_input("Insira um número para Opção 2:")
+else:
+    st.write("Escolha uma opção para liberar um novo campo.")
